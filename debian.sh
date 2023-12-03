31a9d22564df74023a48a09219e35c6f6dcbc605
Willy Micieli <micieli@vivaldi.net>
2023-12-03 11:02:08 +0100

    update scripts fix bug

--- debian.sh
+++ debian.sh
@@ -7,6 +7,3 @@ git clone https://github.com/taishingi/os && cd os || exit 1
 ./os "install" "vim"
 ./os "tree" "vim"
 ./os "remove" "vim"
-./os "list" "installed"
-./os "list" "upgradeable"
-./os "list" "all-versions"
